ac3c7015645171903b12342c968b03e6b53df1c6
Muna Onuorah <jonuorah@ymail.com>
2025-10-21 19:51:09 -0500

    Initial Commit

---
+++ WeatherNow/WeatherNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNowApp.swift
+//  WeatherNow
+//
+//  Created by Muna Onuorah on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
